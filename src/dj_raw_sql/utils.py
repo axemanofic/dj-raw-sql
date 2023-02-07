6f74e0f92644025b5e49886f48ab52a3680a5014
Roman Sotnikov <axeman.ofic@gmail.com>
2023-02-07 13:02:35 +0300

    fix: update 'get_list_ordereddict'

--- src/dj_raw_sql/utils.py
+++ src/dj_raw_sql/utils.py
@@ -2,7 +2,6 @@ from collections import OrderedDict
 from typing import Callable, Optional
 
 from dj_raw_sql.types import ListOrderedDict, RawSQL
-from django.db.backends.utils import CursorWrapper
 
 
 def get_raw_sql(get_query: Callable, *args, **kwargs) -> RawSQL:
@@ -14,7 +13,7 @@ def get_raw_sql(get_query: Callable, *args, **kwargs) -> RawSQL:
     return sql, params
 
 
-def get_list_ordereddict(cursor: CursorWrapper) -> Optional[ListOrderedDict]:
+def get_list_ordereddict(cursor) -> Optional[ListOrderedDict]:
     if cursor.description:
         columns: list[str] = [element[0] for element in cursor.description]
         return [OrderedDict(zip(columns, row)) for row in cursor.fetchall()]
